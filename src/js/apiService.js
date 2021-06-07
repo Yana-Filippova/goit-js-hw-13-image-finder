94ca2b1e264ca5f0c1f1a6c8876bc291a4f96b6f
Yana Filippova <yanafilippova20@gmail.com>
2021-06-07 16:23:53 +0300

    JS HW-13

---
+++ src/js/apiService.js
@@ -0,0 +1,38 @@
+const API_KEY = '21910767-1ff9d0df6204712ecdebd388c';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    return fetch(
+      `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+
+        return hits;
+      })
+      .catch(err => console.log(err));
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/main.js
@@ -0,0 +1,71 @@
+import imagesTpl from '/templates/image.hbs';
+import ImageApiService from '/js/apiService.js';
+import LoadMoreBtn from '/js/load-more-btn.js';
+import { onGalleryItemClick } from '/js/modal.js';
+import getRefs from '/js/get-refs.js';
+import pnotify from '/js/pnotify.js';
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+const refs = getRefs();
+const imageApiService = new ImageApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+refs.imagesContainer.addEventListener('click', onGalleryItemClick);
+
+function onSearch(e) {
+  e.preventDefault();
+  clearImagesContainer();
+
+  imageApiService.query = e.currentTarget.elements.query.value;
+
+  if (imageApiService.query.trim() === '') {
+    loadMoreBtn.disable();
+    return pnotify.noDataEntered();
+  }
+
+  loadMoreBtn.show();
+  imageApiService.resetPage();
+  clearImagesContainer();
+  fetchImages();
+}
+
+function fetchImages() {
+  loadMoreBtn.disable();
+
+  imageApiService.fetchImages().then(images => {
+    appendImagesMarkup(images);
+    loadMoreBtn.enable();
+    scrollPage();
+
+    if (images.length <= 12 && images.length === 0) {
+      loadMoreBtn.hide();
+    }
+    if (images.length === 0) {
+      pnotify.noMatchesFound();
+    }
+  });
+}
+
+function onLoadMore() {
+  fetchImages();
+}
+
+function appendImagesMarkup(images) {
+  refs.imagesContainer.insertAdjacentHTML('beforeend', imagesTpl(images));
+}
+
+function clearImagesContainer() {
+  refs.imagesContainer.innerHTML = '';
+}
+
+function scrollPage() {
+  loadMoreBtn.refs.button.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
